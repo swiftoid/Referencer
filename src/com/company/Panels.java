9fc91adcd767d5053d88f7fb056d9335c0bc7874
Swiftoid <keepitcash@hotmail.com>
2016-01-02 20:58:29 +0000

    Changed form details

--- src/com/company/Panels.java
+++ src/com/company/Panels.java
@@ -124,35 +124,35 @@ public class Panels {
         spacer.setBackground(Color.black);
         inner.add(spacer);
 
-        JLabel Clabel_1 = new JLabel("Enter Author Details");
+        JLabel Clabel_1 = new JLabel("Enter author details:");
         Clabel_1.setFont(mainFont);
         inner.add(Clabel_1);
         JTextField Ctextfield_1 = new JTextField(20);
         Ctextfield_1.setText(this.auto_book_author);
         inner.add(Ctextfield_1);
 
-        JLabel Clabel_2 = new JLabel("Enter Title of Publiction");
+        JLabel Clabel_2 = new JLabel("Enter title of publication:");
         Clabel_2.setFont(mainFont);
         inner.add(Clabel_2);
         JTextField Ctextfield_2 = new JTextField(20);
         Ctextfield_2.setText(this.auto_book_title);
         inner.add(Ctextfield_2);
 
-        JLabel Clabel_3 = new JLabel("Enter Date of Publication");
+        JLabel Clabel_3 = new JLabel("Enter date of publication:");
         Clabel_3.setFont(mainFont);
         inner.add(Clabel_3);
         JTextField Ctextfield_3 = new JTextField(20);
         Ctextfield_3.setText(this.auto_book_date);
         inner.add(Ctextfield_3);
 
-        JLabel Clabel_4 = new JLabel("Enter Publisher");
+        JLabel Clabel_4 = new JLabel("Enter publisher:");
         Clabel_4.setFont(mainFont);
         inner.add(Clabel_4);
         JTextField Ctextfield_4 = new JTextField(20);
         Ctextfield_4.setText(this.auto_book_publisher);
         inner.add(Ctextfield_4);
 
-        JLabel Clabel_5 = new JLabel("Enter Place of Publication");
+        JLabel Clabel_5 = new JLabel("Enter place of publication:");
         Clabel_5.setFont(mainFont);
         inner.add(Clabel_5);
         JTextField Ctextfield_5 = new JTextField(20);
